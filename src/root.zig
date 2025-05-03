ed82a84099e6032876c9cbf989d5e1efa0b890cf
balls <co.saganeli@gmail.com>
2025-05-03 14:43:35 +0400

    pub

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,9 @@
-const ArchetypeManager = @import("archetype_manager.zig").ArchetypeManager;
-const Archetype = @import("archetype.zig").Archetype;
+pub const ArchetypeManager = @import("archetype_manager.zig").ArchetypeManager;
+pub const Archetype = @import("archetype.zig").Archetype;
 
-const ComponentIterator = @import("component_iterator.zig").ComponentIterator;
-const ComponentStorage = @import("component_storage.zig").componentStorage;
+pub const ComponentIterator = @import("component_iterator.zig").ComponentIterator;
+pub const ComponentStorage = @import("component_storage.zig").componentStorage;
 
-const EntityManager = @import("entity_manager.zig").createEntityManager;
+pub const EntityManager = @import("entity_manager.zig").createEntityManager;
 
-const ECSDefinitions = @import("ecs_definitions.zig");
+pub const ECSDefinitions = @import("ecs_definitions.zig");
